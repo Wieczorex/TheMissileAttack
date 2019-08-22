69ea5d6f62beb0c0aa2cb962961e4429db093595
Wieczorex <Mikolaj_Wieczorek@interia.eu>
2019-08-22 17:49:36 +0200

    The game is almost finished

--- Mobile Flappversed Bird/Assets/Scripts/EverythingMovesScript.cs
+++ Mobile Flappversed Bird/Assets/Scripts/EverythingMovesScript.cs
@@ -6,17 +6,26 @@ public class EverythingMovesScript : MonoBehaviour
 {
 	private Transform tr;
 
-	public float predkoscRuszaniaSie;
+	private float predkoscRuszaniaSie = 3;
 
     // Start is called before the first frame update
     void Start()
     {
+		predkoscRuszaniaSie = 3;
 		tr = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 		tr.Translate(Vector3.left * predkoscRuszaniaSie * Time.deltaTime);
+		if (GameControl.score >= 50)
+		{
+			predkoscRuszaniaSie = 3.5f;
+		}
+		if (GameControl.score >= 200)
+		{
+			predkoscRuszaniaSie = 4;
+		}
     }
 }
